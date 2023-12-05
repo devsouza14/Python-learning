a34677e4c31b23220931105abbebf79b79eb8382
Erique Souza <donsouza14@gmail.com>
2023-12-04 21:49:11 -0300

    Variaveis e constantes

---
+++ variaveis_constantes.py
@@ -0,0 +1,11 @@
+nome = "Erique"
+idade = 32
+
+nome, idade = "Mario", 63
+print(nome, idade)
+
+limite_de_saque_diario = 1000
+
+BRAZILIAN_STATES = ["SP", "MG", "PR", "SC"]
+
+print(limite_de_saque_diario, BRAZILIAN_STATES)
